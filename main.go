26575711c03ef3496d21fb090da50749da86cf27
Lee Hyohwak <crewe1746@naver.com>
2023-05-06 13:58:11 +0900

    Open connection of discord bot

--- main.go
+++ main.go
@@ -1,9 +1,56 @@
 package main
 
 import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered:", r)
+		}
+	}()
 
+	session, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = session.Open(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Bot is running!")
+
+	stop := make(chan struct{})
+	shutdown := make(chan os.Signal, 1)
+
+	signal.Notify(shutdown,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+
+	go func() {
+		defer func() {
+			close(shutdown)
+			close(stop)
+		}()
+		<-shutdown
+	}()
+
+	<-stop
+
+	if err = session.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Bot is stopped!")
 }
