236e6a81731fc552a1d85ddf078ce1132870296f
Lee Hyohwak <crewe1746@naver.com>
2023-05-08 20:15:30 +0900

    Implement start presentation handler

--- cmd/study/admin.go
+++ cmd/study/admin.go
@@ -382,7 +382,51 @@ func (b *StudyBot) closeSubmissionHandler(s *discordgo.Session, i *discordgo.Int
 	}
 }
 
-func (b *StudyBot) startPresentationHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {}
+func (b *StudyBot) startPresentationHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	guild := b.svc.GetGuildID()
+
+	// check if the command is executed in the correct guild
+	if guild != i.GuildID {
+		// TODO: error handling
+		log.Println("wrong guild")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// start presentation
+	err := b.svc.StartPresentation(ctx, i.Member.User.ID)
+
+	if err != nil {
+		// TODO: error handling
+		log.Println(err)
+		return
+	}
+
+	noticeCh := b.svc.GetNoticeChannelID()
+
+	// send a notice message
+	_, err = s.ChannelMessageSendEmbed(noticeCh, EmbedTemplate(s.State.User, "발표 시작", "발표가 시작되었습니다."))
+	if err != nil {
+		// TODO: error handling
+		log.Println(err)
+		return
+	}
+
+	// send a response message
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "발표가 시작되었습니다.",
+			Flags:   discordgo.MessageFlagsEphemeral,
+		},
+	})
+	if err != nil {
+		// TODO: error handling
+		log.Println(err)
+	}
+}
 
 func (b *StudyBot) endPresentationHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {}
 
