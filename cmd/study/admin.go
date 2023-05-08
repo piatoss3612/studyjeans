e9d550bda92cfd91b3a6a91728f31ec4fa89d0ce
Lee Hyohwak <crewe1746@naver.com>
2023-05-08 20:20:41 +0900

    Implement start feedback handler

--- cmd/study/admin.go
+++ cmd/study/admin.go
@@ -473,7 +473,50 @@ func (b *StudyBot) endPresentationHandler(s *discordgo.Session, i *discordgo.Int
 	}
 }
 
-func (b *StudyBot) startFeedbackHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {}
+func (b *StudyBot) startFeedbackHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	guild := b.svc.GetGuildID()
+
+	// check if the command is executed in the correct guild
+	if guild != i.GuildID {
+		// TODO: error handling
+		log.Println("wrong guild")
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// start feedback
+	err := b.svc.StartReview(ctx, i.Member.User.ID)
+	if err != nil {
+		// TODO: error handling
+		log.Println(err)
+		return
+	}
+
+	noticeCh := b.svc.GetNoticeChannelID()
+
+	// send a notice message
+	_, err = s.ChannelMessageSendEmbed(noticeCh, EmbedTemplate(s.State.User, "피드백 시작", "피드백이 시작되었습니다."))
+	if err != nil {
+		// TODO: error handling
+		log.Println(err)
+		return
+	}
+
+	// send a response message
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "피드백이 시작되었습니다.",
+			Flags:   discordgo.MessageFlagsEphemeral,
+		},
+	})
+	if err != nil {
+		// TODO: error handling
+		log.Println(err)
+	}
+}
 
 func (b *StudyBot) endFeedbackHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {}
 
