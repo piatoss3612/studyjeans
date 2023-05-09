dcbfdbc9c08e1b5d449ecf76c508d7dcea0a9f4e
Lee Hyohwak <crewe1746@naver.com>
2023-05-09 18:12:54 +0900

    Refactor main.go

--- cmd/study/main.go
+++ cmd/study/main.go
@@ -11,13 +11,23 @@ import (
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/piatoss3612/presentation-helper-bot/internal/db"
 	"github.com/piatoss3612/presentation-helper-bot/internal/service/study"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
 
+var sugar *zap.SugaredLogger
+
 func main() {
+	logger, _ := zap.NewProduction()
+	defer func() {
+		_ = logger.Sync()
+	}()
+
+	sugar = logger.Sugar()
+
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println("Recovered:", r)
+			sugar.Info("Panic recovered", "error", r)
 		}
 	}()
 
@@ -27,44 +37,24 @@ func main() {
 }
 
 func run() {
-	logger, _ := zap.NewProduction()
-	defer func() {
-		_ = logger.Sync()
-	}()
-
-	sugar := logger.Sugar()
-
-	cfg, err := NewConfig(os.Getenv("CONFIG_FILE"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	cfg := mustLoadConfig(os.Getenv("CONFIG_FILE"))
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	mongoClient, err := db.ConnectMongoDB(ctx, cfg.MongoURI)
-	if err != nil {
-		log.Fatal(err)
-	}
+	mongoClient := mustConnectMongoDB(ctx, cfg.MongoURI)
 	defer func() {
 		_ = mongoClient.Disconnect(context.Background())
-		log.Println("Disconnected from MongoDB!")
+		sugar.Info("Disconnected from MongoDB!")
 	}()
 
-	log.Println("Connected to MongoDB!")
+	sugar.Info("Connected to MongoDB!")
 
-	svc, err := study.NewService(ctx, study.NewTx(mongoClient, cfg.DBName),
-		cfg.GuildID, cfg.ManagerID, cfg.NoticeChannelID)
-	if err != nil {
-		log.Fatal(err)
-	}
+	svc := mustInitStudyService(ctx, study.NewTx(mongoClient, cfg.DBName), cfg.GuildID, cfg.ManagerID, cfg.NoticeChannelID)
 
-	log.Println("Study service is ready!")
+	sugar.Info("Study service is ready!")
 
-	sess, err := discordgo.New("Bot " + cfg.BotToken)
-	if err != nil {
-		log.Fatal(err)
-	}
+	sess := mustOpenDiscordSession(cfg.BotToken)
 
 	bot := NewStudyBot(sess, svc, sugar).Setup()
 
@@ -74,10 +64,10 @@ func run() {
 	}
 	defer func() {
 		_ = bot.Close()
-		log.Println("Disconnected from Discord!")
+		sugar.Info("Disconnected from Discord!")
 	}()
 
-	log.Println("Connected to Discord!")
+	sugar.Info("Connected to Discord!")
 
 	<-stop
 }
@@ -85,8 +75,44 @@ func run() {
 func mustSetTimezone(tz string) {
 	loc, err := time.LoadLocation(tz)
 	if err != nil {
-		log.Fatal(err)
+		sugar.Fatal(err)
 	}
 
 	time.Local = loc
+}
+
+func mustLoadConfig(path string) *Config {
+	cfg, err := NewConfig(path)
+	if err != nil {
+		sugar.Fatal(err)
+	}
+
+	return cfg
+}
+
+func mustConnectMongoDB(ctx context.Context, uri string) *mongo.Client {
+	mongoClient, err := db.ConnectMongoDB(ctx, uri)
+	if err != nil {
+		sugar.Fatal(err)
+	}
+
+	return mongoClient
+}
+
+func mustInitStudyService(ctx context.Context, tx study.Tx, guildID, managerID, noticeChID string) study.Service {
+	svc, err := study.NewService(ctx, tx, guildID, managerID, noticeChID)
+	if err != nil {
+		sugar.Fatal(err)
+	}
+
+	return svc
+}
+
+func mustOpenDiscordSession(token string) *discordgo.Session {
+	sess, err := discordgo.New("Bot " + token)
+	if err != nil {
+		sugar.Fatal(err)
+	}
+
+	return sess
 }
