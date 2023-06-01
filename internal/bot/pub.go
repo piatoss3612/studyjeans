69a13b1e634dc00ad8514bbd5179c0dab65e3776
Lee Hyohwak <crewe1746@naver.com>
2023-06-01 15:14:30 +0900

    Remove unnecessary files

--- internal/bot/pub.go
+++ internal/bot/pub.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/piatoss3612/presentation-helper-bot/internal/event"
 	"github.com/piatoss3612/presentation-helper-bot/internal/study"
 )
 
@@ -16,20 +15,6 @@ func (b *StudyBot) publishRound(topic string, round *study.Round) {
 		err := b.pub.Publish(ctx, topic, round)
 		if err != nil {
 			b.sugar.Errorw(err.Error(), "event", "publish round", "topic", topic, "try", i+1)
-			continue
-		}
-		return
-	}
-}
-
-func (b *StudyBot) publishEvent(evt event.Event) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	for i := 0; i < 10; i++ {
-		err := b.pub.Publish(ctx, evt.Topic(), evt)
-		if err != nil {
-			b.sugar.Errorw(err.Error(), "event", "publish event", "topic", evt.Topic(), "try", i+1)
 			continue
 		}
 		return
