45e62cc16c6957da2bdb1b107ef110cae133a1d0
Lee Hyohwak <crewe1746@naver.com>
2023-06-01 12:57:46 +0900

    Define command registerer type

---
+++ internal/study/bot/command/command.go
@@ -0,0 +1,31 @@
+package command
+
+import "github.com/bwmarrin/discordgo"
+
+type Registerer interface {
+	Register(cmd discordgo.ApplicationCommand, fn HandleFunc)
+	Commands() []*discordgo.ApplicationCommand
+}
+
+type HandleFunc func(s *discordgo.Session, i *discordgo.InteractionCreate)
+
+type commandRegisterer struct {
+	cmds  []*discordgo.ApplicationCommand
+	funcs map[string]HandleFunc
+}
+
+func NewRegisterer() Registerer {
+	return &commandRegisterer{
+		cmds:  []*discordgo.ApplicationCommand{},
+		funcs: make(map[string]HandleFunc),
+	}
+}
+
+func (r *commandRegisterer) Register(cmd discordgo.ApplicationCommand, fn HandleFunc) {
+	r.cmds = append(r.cmds, &cmd)
+	r.funcs[cmd.Name] = fn
+}
+
+func (r *commandRegisterer) Commands() []*discordgo.ApplicationCommand {
+	return r.cmds
+}
