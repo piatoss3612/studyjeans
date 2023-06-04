3b6d52117dd250c40c0b8f582f2ec14bf8cb39bf
Lee Hyohwak <crewe1746@naver.com>
2023-06-04 15:38:25 +0900

    Add error event

---
+++ internal/error/event.go
@@ -0,0 +1,46 @@
+package error
+
+import (
+	"errors"
+	"time"
+)
+
+type EventTopic string
+
+var EventTopicError EventTopic = "error"
+
+var ErrUnknownEventTopic = errors.New("unknown event topic")
+
+func (t EventTopic) Validate() error {
+	switch t {
+	case EventTopicError:
+	default:
+		return ErrUnknownEventTopic
+	}
+
+	return nil
+}
+
+func (t EventTopic) String() string {
+	return string(t)
+}
+
+type Event struct {
+	Topic       EventTopic `json:"topic"`
+	Description string     `json:"description"`
+	Timestamp   int64      `json:"timestamp"`
+}
+
+func NewEvent(topic EventTopic, description string) (Event, error) {
+	if err := topic.Validate(); err != nil {
+		return Event{}, err
+	}
+
+	evt := Event{
+		Topic:       topic,
+		Description: description,
+		Timestamp:   time.Now().Unix(),
+	}
+
+	return evt, nil
+}
