4013ca7de381c8ea8d405550e131d289de7c81c5
Lee Hyohwak <crewe1746@naver.com>
2023-10-14 10:51:20 +0900

    Add test for bot package and run stub bot in main.go

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -2,33 +2,22 @@ package bot
 
 import "github.com/bwmarrin/discordgo"
 
-type Bot interface {
-	AddHandler(h interface{})
-	Open() error
-	Close() error
-}
-
-type DiscordBot struct {
+type Bot struct {
 	s *discordgo.Session
 }
 
-func NewDiscordBot(token string) (*DiscordBot, error) {
-	s, err := discordgo.New("Bot " + token)
-	if err != nil {
-		return nil, err
-	}
-
-	return &DiscordBot{s: s}, nil
+func New(s *discordgo.Session) *Bot {
+	return &Bot{s: s}
 }
 
-func (b *DiscordBot) AddHandler(h interface{}) {
+func (b *Bot) AddHandler(h interface{}) {
 	b.s.AddHandler(h)
 }
 
-func (b *DiscordBot) Open() error {
+func (b *Bot) Open() error {
 	return b.s.Open()
 }
 
-func (b *DiscordBot) Close() error {
+func (b *Bot) Close() error {
 	return b.s.Close()
 }
--- pkg/bot/bot_test.go
+++ pkg/bot/bot_test.go
@@ -1,9 +1,49 @@
 package bot
 
 import (
+	"os"
 	"testing"
+
+	"github.com/bwmarrin/discordgo"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func TestNewDiscordBot(t *testing.T) {
+	s := &discordgo.Session{}
+	bot := New(s)
+	if bot.s != s {
+		t.Errorf("Expected %v, got %v", s, bot.s)
+	}
+}
 
+func TestDiscordBot_AddHandler(t *testing.T) {
+	s := &discordgo.Session{}
+	bot := New(s)
+	bot.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {})
+}
+
+func TestDiscordBot_Open(t *testing.T) {
+	s, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
+
+	b := New(s)
+
+	if err := b.Open(); err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
+}
+
+func TestDiscordBot_Close(t *testing.T) {
+	s, err := discordgo.New("Bot " + os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
+
+	b := New(s)
+
+	if err := b.Close(); err != nil {
+		t.Errorf("Expected nil, got %v", err)
+	}
 }
